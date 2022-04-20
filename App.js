9715f20ba8aa4e2d0c771c8e20725d87d9dfa41b
David Surya Ardi <david@192.168.1.8>
2022-04-21 01:45:19 +0700

    err close modal

--- App.js
+++ App.js
@@ -1,38 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal } from 'react-native';
+import React, { useState } from 'react';
 import colors from './Color';
 import { AntDesign } from "@expo/vector-icons";
 import tempData from './tempData';
 import TodoList from './components/TodoList';
+import AddListModal from './components/addListModal';
 
 export default function App() {
-      return (
-      <View style={styles.container}>
-        <View style={{ flexDirection: 'row'}}>
-          <View style={styles.divider} />
-          <Text style={styles.title}>
-            Todo < Text style={{fontWeight: "300", color: colors.blue}}>Lists</Text>
-          </Text>
-          <View style={styles.divider} />
-        </View>
+  // state = {
+  //   addTodoVisible: false
+  // };
 
-        <View style={{ marginVertical: 48 }}>
-          <TouchableOpacity style={styles.addList}>
-            <AntDesign name='plus' size={15} color={colors.blue} />
-          </TouchableOpacity>
+  // function toggleAddTodoModal() { 
+  //   this.setState({addTodoVisible: !this.state.addTodoVisible})
+  // }
+  const [addTodoVisible, setAddTodoVisible] = useState(false)
+  function toggleAddTodoVisible (){
+    setAddTodoVisible(true)
+  }
 
-          <Text style={styles.add}>Add List</Text>
-        </View>
+  function toggleModalVisible (){
+    setAddTodoVisible(false)
+  }
 
-        <View style={{height: 275, paddingLeft: 32}}>
-          <FlatList 
-          data={tempData} 
-          keyExtractor={item => item.name}
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          renderItem={({ item }) => <TodoList list={item} />}
-          />
+  return (
+    <View style={styles.container}>
+      <Modal 
+        animationType='slide' 
+        visible={addTodoVisible}
+        // onRequestClose={() => toggleAddTodoVisible()}
+      >
+        <AddListModal closeModal= {() =>
+        toggleModalVisible()} />
+      </Modal>
+      <View style={{ flexDirection: 'row'}}>
+        <View style={styles.divider} />
+        <Text style={styles.title}>
+          Todo < Text style={{fontWeight: "300", color: colors.blue}}>Lists</Text>
+        </Text>
+        <View style={styles.divider} />
+      </View>
+
+      <View style={{ marginVertical: 48 }}>
+        <TouchableOpacity style={styles.addList} onPress={() => toggleAddTodoVisible()}>
+          <AntDesign name='plus' size={15} color={colors.blue} />
+        </TouchableOpacity>
+
+        <Text style={styles.add}>Add List</Text>
+      </View>
+
+      <View style={{height: 275, paddingLeft: 32}}>
+        <FlatList 
+        data={tempData} 
+        keyExtractor={item => item.name}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => <TodoList list={item} />}
+        />
         </View>
       </View>
     );
---
+++ components/addListModal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import colors from '../Color';
+
+export default function AddListModal(closeModal) {
+    return (
+        <KeyboardAvoidingView style={styles.container} behavior='padding'>
+            <TouchableOpacity style={{position: 'absolute', top: 64, right: 32}} onPress={closeModal} >
+                <AntDesign name='close' size={24} color={colors.black} />
+            </TouchableOpacity>
+        </KeyboardAvoidingView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})

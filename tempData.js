a457113c13bf33b3837e341964d57e7499f63c76
David Surya Ardi <david@192.168.1.8>
2022-04-20 23:23:02 +0700

    container fe

---
+++ tempData.js
@@ -0,0 +1,90 @@
+export default tempData = [
+    {
+        name: 'Hangout with my Friends',
+        color: '#24A6D9',
+        todos: [
+            {
+                title: 'Coffee Shop',
+                completed: false
+            },
+            {
+                title: 'Party',
+                completed: false
+            },
+            {
+                title: 'Restaurant',
+                completed: false
+            },
+            {
+                title: 'Skates',
+                completed: true
+            }
+        ]
+    },
+    {
+        name: 'Trip',
+        color: '#24A6D9',
+        todos: [
+            {
+                title: 'Luggage',
+                completed: false
+            },
+            {
+                title: 'Passport',
+                completed: true
+            },
+            {
+                title: 'Hotel',
+                completed: false
+            },
+            {
+                title: 'Book',
+                completed: true
+            }
+        ]
+    },
+    {
+        name: 'Birthday',
+        color: '#24A6D9',
+        todos: [
+            {
+                title: 'Balloons',
+                completed: false
+            },
+            {
+                title: 'Invitation',
+                completed: true
+            },
+            {
+                title: 'Hotel',
+                completed: false
+            },
+            {
+                title: 'Cake',
+                completed: true
+            }
+        ]
+    },
+    {
+        name: 'Daily',
+        color: '#24A6D9',
+        todos: [
+            {
+                title: 'Take dog for a walk',
+                completed: false
+            },
+            {
+                title: 'Coding',
+                completed: true
+            },
+            {
+                title: 'Editing',
+                completed: false
+            },
+            {
+                title: 'Freelance',
+                completed: true
+            }
+        ]
+    },
+]
